5180dffd5225322df3750b473252dc55d8514335
AndyLauCD <chendi@radi.ac.cn>
2018-06-10 22:35:09 +0800

    180610

---
+++ LeetCode/1806/180610letter_combinations_of_a_phone_number.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# @StartTime : 2018/6/10 12:25
+# @EndTime : 2018/6/10 12:25
+# @Author  : Andy
+# @Site    : https://leetcode.com/problems/letter-combinations-of-a-phone-number/discuss/8063/Python-solution
+# @File    : 180610letter_combinations_of_a_phone_number.py
+# @Software: PyCharm
+
+
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+"""
+
+w
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        interpret_digit = {
+            '1': '',
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+            '0': ' '}
+        all_combinations = [''] if digits else []
+        for digit in digits:
+            current_combinations = list()
+            for letter in interpret_digit[digit]:
+                for combination in all_combinations:
+                    current_combinations.append(combination + letter)
+            all_combinations = current_combinations
+        return all_combinations
+
+print(Solution().letterCombinations('23'))
